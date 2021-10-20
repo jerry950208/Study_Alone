4ab7fa23f21dc3530ec7fff01eab5f1e11ed28a9
jerry950208 <jerry950208@naver.com>
2021-10-20 22:53:50 +0900

    Study_Alone

---
+++ C++/study_for_vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void){
+	
+	int n;
+	vector<int> v;
+	vector<vector<int> > vv;
+
+	for(int i=0; i<5; i++){
+		
+	}
+
+	return 0;
+}
