997182f3745881416760f06e25e00f5a8c1acca8
jerry950208 <jerry950208@naver.com>
2021-11-10 16:47:14 +0900

    Online_lecture

---
+++ JAVA/Online_lecture/HelloWorld.java
@@ -0,0 +1,10 @@
+//자바는 파일명 하고 클래스명 하고 똑같아야 한다(대문자도 같아야한다, 클래스명의 첫번째 글자는 대문자로 저장하기 -> 관례적으로 대문자 사용)
+public class HelloWorld{
+	//String의 첫번째 글자도 대문자로 해주기
+	public static void main(String[] args){
+		//System의 첫번째 글자도 대문자로 해주기
+		System.out.println("Hello World~");	//문자열은 쌍 따옴표로 출력
+	}
+}
+
+
---
+++ JAVA/Online_lecture/Hello_Java.java
@@ -0,0 +1,39 @@
+public class Hello_Java{
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello Java");
+	}
+}
+
+/*
+접근제한자	클래스선언	클래스이름{
+	접근제한자  static  반환타입  매서드명(파라미터){
+		//구현할 코드 작성
+	}
+}
+
+
+(1) 접근제한자(접근제어자) --> public, private. protected, default
+				public : 아무나 접근 가능
+				
+				*클래스나 매서드에 접근할 수 있는 범위를 지정해 주는 것이다
+				*private > protected > public
+
+
+(2) 클래스 선언 --> class
+				
+				*객체를 생성하는 틀, 프레임, 공장, 템플릿
+
+
+
+(3) 클래스 이릅 --> 카멜케이스(단어와 단어사이의 구분 -> 대문자 사용) ex) HelloWorld
+
+
+(4) 매서드 이름 --> 매서드 = 함수(어떤 특정한 동작이나 작업, 행위 등을 수행하는 것)
+
+
+(5) 파라미터 --> 매개변수
+
+
+*/
