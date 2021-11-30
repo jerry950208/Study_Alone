1e658310e11c55c4b0da9eadfe59cbe3d4a53126
jerry950208 <jerry950208@naver.com>
2021-11-30 13:15:29 +0900

    Method_MethodCall1

---
+++ JAVA/Online_lecture/Java100_part_2/Method_MethodCall1.java
@@ -0,0 +1,23 @@
+// 메서드로 인자 전달시 정수 100을 보냈을때 아래 코드의 결과를 말해보시오
+// 이 문제는 자바의 기본형 타입과 참조형 타입의 다양한 메서드 호출 방식에 대해서 아는지를 묻는 문제이다
+
+public class Method_MethodCall1{
+	
+
+	//[!] : Call by value --> 값에 의한 호출 --> 값에 의해서 (메서드를) 호출*** --> [!] : Call by teference --> 주소에 의한 호출 --> 주소에 의해서 호출
+	// 메서드로 인자값을 넘길 때 해당 값을 복사하여 넘기는 방식 --> 따라서sum() 내부에서는 복사된 값으로 처리를 한다
+	public static void sum(int a){
+		a += 400;
+		System.out.println(a); 		//500
+	}
+
+	public static void main(String[] args){
+
+		//[1] : 변수 선언 및 메서드 호출
+		int a = 100;
+		sum(a);
+
+		//[2] : 출력
+		System.out.println(a);		//100
+	}
+}
